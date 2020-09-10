482bac80092f4164b11f99ac76027ae12c3ea7a9
Taylor Phillips <taylorscottphillips@gmail.com>
2020-09-10 10:16:42 -0500

    Add basic tests for Patron class

---
+++ test/patron_test.rb
@@ -0,0 +1,19 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/patron'
+
+class PatronTest < Minitest::Test
+  def test_it_exists
+    patron_1 = Patron.new("Bob", 20)
+
+    assert_instance_of Patron, patron_1
+  end
+
+  def test_it_has_attributes
+    patron_1 = Patron.new("Bob", 20)
+
+    assert_equal 'Bob', patron_1.name
+    assert_equal 20, patron_1.spending_money
+    assert_equal [], patron_1.interests
+  end
+end
