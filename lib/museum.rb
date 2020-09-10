0f53d0e4dd61ad3b86803118ae4463818ea96b54
Taylor Phillips <taylorscottphillips@gmail.com>
2020-09-10 10:50:34 -0500

    Create recommend_exhibits method in Museum class

--- lib/museum.rb
+++ lib/museum.rb
@@ -9,4 +9,10 @@ class Museum
   def add_exhibit(exhibit)
     exhibits << exhibit
   end
+
+  def recommend_exhibits(patron)
+    exhibits.find_all do |exhibit|
+      patron.interests.include?(exhibit.name)
+    end
+  end
 end
