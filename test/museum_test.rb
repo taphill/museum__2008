eb36b2bcbfa19d3fc10616bf4f8dbd38efa4064f
Taylor Phillips <taylorscottphillips@gmail.com>
2020-09-10 10:34:46 -0500

    Add test for add_exhibits method in Museum class

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -17,4 +17,20 @@ class MuseumTest  < Minitest::Test
     assert_equal "Denver Museum of Nature and Science", dmns.name
     assert_equal [], dmns.exhibits
   end
+
+  def test_it_can_add_exhibits
+    dmns = Museum.new("Denver Museum of Nature and Science")
+
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    imax = Exhibit.new({name: "IMAX",cost: 15})
+
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+
+    expected = [gems_and_minerals, dead_sea_scrolls, imax]
+
+    assert_equal expected, dmns.exhibits
+  end
 end
