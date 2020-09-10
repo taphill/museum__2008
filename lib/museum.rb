2db7402a28d94ebc602860e530c1ab2a14b6d937
Taylor Phillips <taylorscottphillips@gmail.com>
2020-09-10 11:15:52 -0500

    Create patrons_by_exhibit_interest method in Museum class

--- lib/museum.rb
+++ lib/museum.rb
@@ -20,4 +20,22 @@ class Museum
   def admit(patron)
     patrons << patron
   end
+
+  def patrons_by_exhibit_interest
+    exhibit_interest = {}
+
+    exhibits.each do |exhibit|
+      exhibit_interest[exhibit] = find_patrons_interested(exhibit)
+    end
+
+    exhibit_interest
+  end
+
+  private
+
+  def find_patrons_interested(exhibit)
+    patrons.find_all do |patron|
+      patron.interests.include?(exhibit.name)
+    end
+  end
 end
